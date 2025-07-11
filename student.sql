c84243c0d7631312796b06fdb66dc294aa1ea3f1
unknown <chiranjeevi615@gmail.com>
2025-07-11 21:19:10 +0530

    first commit

---
+++ student.sql
@@ -0,0 +1,13 @@
+SELECT `student`.`id`,
+    `student`.`created_date`,
+    `student`.`father_name`,
+    `student`.`father_phone`,
+    `student`.`first_name`,
+    `student`.`last_name`,
+    `student`.`mother_name`,
+    `student`.`mother_phone`,
+    `student`.`national_id`,
+    `student`.`student_number`,
+    `student`.`updated_date`,
+    `student`.`classroom_id`
+FROM `test`.`student`;
